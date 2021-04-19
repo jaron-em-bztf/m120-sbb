f416bb846cd23657e8f29f20247de0035397d125
Jaron Emmenegger <jaron.emmenegger@gmail.com>
2021-04-19 08:38:34 +0200

    RouteView prop handling

--- js/RouteView.js
+++ js/RouteView.js
@@ -3,6 +3,15 @@ class RouteView
     constructor(switchTemplate)
     {
         Modal.$title.html("Strecke");
+        this.setupPropHandling();
         switchTemplate(ModalTemplates.routeTemplate.$body);
+    }
+
+    setupPropHandling()
+    {
+        ModalTemplates.routeTemplate.$from.change(() =>
+            ModalProps.RouteProps.from = ModalTemplates.routeTemplate.$from.val());
+        ModalTemplates.routeTemplate.$to.change(() =>
+            ModalProps.RouteProps.to = ModalTemplates.routeTemplate.$to.val());
     }
 }
